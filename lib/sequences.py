96604a982a8bf68824fcf5ba3f7665a1b709991c
Maria Jenkins <macjenkin@gmail.com>
2023-10-03 02:18:42 -0500

    complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+    for x in range(length):
+        if x == 0:
+            sequence.append(0)
+        elif x == 1:
+            sequence.append(1)
+        else:
+            sequence.append(sequence[-1] + sequence[-2])
+    print(sequence)
+        
+    
+        
